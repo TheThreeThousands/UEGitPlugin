5748f92acd61f49a2306ba0bc645a6d69cf3ee3b
Darcy Abell <Darcy@thethreethousands.com>
2024-11-07 16:33:43 +1100

    Revert "Allow newly added files to be deleted"
    This reverts commit 448b2fd7e57642aaad1117efecebe565b57778c9.

--- Source/GitSourceControl/Private/GitSourceControlState.cpp
+++ Source/GitSourceControl/Private/GitSourceControlState.cpp
@@ -365,11 +365,6 @@ bool FGitSourceControlState::CanEdit() const
 
 bool FGitSourceControlState::CanDelete() const
 {
-	if (IsAdded())
-	{
-		return true;
-	}
-	
 	// Perforce enforces that a deleted file must be current.
 	if (!IsCurrent())
 	{
